b0ac3ce26de5ef91778b1ad45cf6cf2c58b4f649
ililim <ililim@nomail.test>
2018-07-28 01:33:09 -0700

    Remove unnecessary usings

--- SituationAutomation/SituationAutomation.cs
+++ SituationAutomation/SituationAutomation.cs
@@ -6,7 +6,6 @@ using IlilimModUtils;
 using Partiality.Modloader;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Reflection;
 using System.Text.RegularExpressions;
